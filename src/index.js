a6ad96a132e486c6f272e31027604ef653156d0b
GGalina <71042425+GGalina@users.noreply.github.com>
2023-04-26 22:28:28 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import './index.scss';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/finance'>
+        <BrowserRouter basename='/Finance'>
           <App />
         </BrowserRouter>
       </PersistGate>
